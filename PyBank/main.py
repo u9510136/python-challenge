565412473ade7f0aebe3cfe117d9d8489dfc4cf6
Hsuan-Yu Lin <o100249243@gmail.com>
2018-12-09 12:53:19 -0700

    Python Bank challenge added

---
+++ PyBank/main.py
@@ -0,0 +1,54 @@
+import os
+import csv
+
+month_count = 0
+total_sum = 0
+inc_max_month = ""
+dec_min_month = ""
+inc_max = 0
+dec_min = 0 
+
+bank_csv = os.path.join("budget_data.csv")
+
+with open(bank_csv, "r", encoding = "UTF-8") as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
+
+    for row in csvreader:
+
+        # print(row)
+        month_count += 1
+        total_sum += int(row[1])
+
+        if int(row[1]) > inc_max:
+            
+            inc_max = int(row[1])
+            inc_max_month = row[0]
+
+        elif int(row[1]) < dec_min:
+
+            dec_min = int(row[1])
+            dec_min_month = row[0]
+    
+    print("Financial Analysis")
+    print("---------------------------------")
+    print("Total months: " + str(month_count))
+    print("Total: $" + str(total_sum))
+    print("Average Change: $" + str(round(total_sum/month_count, 2)))
+    print("Greatest Increase in Profits: " + inc_max_month + " ($" + str(inc_max) + ")")
+    print("Greatest Decrease in Profits: " + dec_min_month + " ($" + str(dec_min) + ")")
+
+    op_path = os.path.join("finacial_analysis.csv")
+
+    with open(op_path, "w", newline = "", encoding = "UTF-8") as datafile:
+
+        writer = csv.writer(datafile, delimiter=',')
+
+        writer.writerow(["Financial Analysis"])
+        writer.writerow(["Totla months", month_count])
+        writer.writerow(["Total", total_sum])
+        writer.writerow(["Average Change",round(total_sum/month_count, 2)])
+        writer.writerow(["Greatest Increase in Profits",inc_max_month,inc_max])
+        writer.writerow(["Greatest Decrease in Profits",dec_min_month,dec_min])
+        
