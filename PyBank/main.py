7119ae478313a282cc4676a0cdbc3c1a03bcdcea
Hsuan-Yu Lin <o100249243@gmail.com>
2018-12-09 14:55:06 -0700

    commit for bank data added

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,6 +8,7 @@ dec_min_month = ""
 inc_max = 0
 dec_min = 0 
 
+# Read Data from .csv file
 bank_csv = os.path.join("budget_data.csv")
 
 with open(bank_csv, "r", encoding = "UTF-8") as csvfile:
@@ -17,20 +18,20 @@ with open(bank_csv, "r", encoding = "UTF-8") as csvfile:
 
     for row in csvreader:
 
-        # print(row)
         month_count += 1
-        total_sum += int(row[1])
+        total_sum += int(row[1]) # Count for set of data
 
         if int(row[1]) > inc_max:
             
             inc_max = int(row[1])
-            inc_max_month = row[0]
+            inc_max_month = row[0] # find the greatest increase 
 
         elif int(row[1]) < dec_min:
 
             dec_min = int(row[1])
-            dec_min_month = row[0]
-    
+            dec_min_month = row[0] # find the greatest decrease
+
+#print out the result    
 print("Financial Analysis")
 print("---------------------------------")
 print("Total months: " + str(month_count))
@@ -39,6 +40,7 @@ print("Average Change: $" + str(round(total_sum/month_count, 2)))
 print("Greatest Increase in Profits: " + inc_max_month + " ($" + str(inc_max) + ")")
 print("Greatest Decrease in Profits: " + dec_min_month + " ($" + str(dec_min) + ")")
 
+#export the result into a new csv file
 op_path = os.path.join("finacial_analysis.csv")
 
 with open(op_path, "w", newline = "", encoding = "UTF-8") as datafile:
