ea0b39fe7e4c9ebefd4ed009c42b0f80c49c1cdf
Hsuan-Yu Lin <o100249243@gmail.com>
2018-12-09 14:48:50 -0700

    Python election data challange added

--- PyBank/main.py
+++ PyBank/main.py
@@ -31,24 +31,23 @@ with open(bank_csv, "r", encoding = "UTF-8") as csvfile:
             dec_min = int(row[1])
             dec_min_month = row[0]
     
-    print("Financial Analysis")
-    print("---------------------------------")
-    print("Total months: " + str(month_count))
-    print("Total: $" + str(total_sum))
-    print("Average Change: $" + str(round(total_sum/month_count, 2)))
-    print("Greatest Increase in Profits: " + inc_max_month + " ($" + str(inc_max) + ")")
-    print("Greatest Decrease in Profits: " + dec_min_month + " ($" + str(dec_min) + ")")
+print("Financial Analysis")
+print("---------------------------------")
+print("Total months: " + str(month_count))
+print("Total: $" + str(total_sum))
+print("Average Change: $" + str(round(total_sum/month_count, 2)))
+print("Greatest Increase in Profits: " + inc_max_month + " ($" + str(inc_max) + ")")
+print("Greatest Decrease in Profits: " + dec_min_month + " ($" + str(dec_min) + ")")
 
-    op_path = os.path.join("finacial_analysis.csv")
+op_path = os.path.join("finacial_analysis.csv")
 
-    with open(op_path, "w", newline = "", encoding = "UTF-8") as datafile:
+with open(op_path, "w", newline = "", encoding = "UTF-8") as datafile:
 
-        writer = csv.writer(datafile, delimiter=',')
+    writer = csv.writer(datafile, delimiter=',')
 
-        writer.writerow(["Financial Analysis"])
-        writer.writerow(["Totla months", month_count])
-        writer.writerow(["Total", total_sum])
-        writer.writerow(["Average Change",round(total_sum/month_count, 2)])
-        writer.writerow(["Greatest Increase in Profits",inc_max_month,inc_max])
-        writer.writerow(["Greatest Decrease in Profits",dec_min_month,dec_min])
-        
+    writer.writerow(["Financial Analysis"])
+    writer.writerow(["Totla months", month_count])
+    writer.writerow(["Total", total_sum])
+    writer.writerow(["Average Change",round(total_sum/month_count, 2)])
+    writer.writerow(["Greatest Increase in Profits",inc_max_month,inc_max])
+    writer.writerow(["Greatest Decrease in Profits",dec_min_month,dec_min])
---
+++ PyRoll/main.py
@@ -0,0 +1,59 @@
+import os
+import csv
+
+total_vote = 0
+can_name = ["Khan","Correy","Li","O'Tooley"]
+can_vote = [0,0,0,0]
+can_percent = [0,0,0,0]
+
+vote_csv = os.path.join("election_data.csv")
+
+with open(vote_csv, "r", encoding = "UTF-8") as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
+
+    for row in csvreader:
+
+       # print(row)
+      total_vote += 1
+      if row[2] == can_name[0]:
+        can_vote[0] += 1
+      elif row[2] == can_name[1]:
+        can_vote[1] += 1
+      elif row[2] == can_name[2]:
+        can_vote[2] += 1
+      else:
+        can_vote[3] += 1
+
+if max(can_vote) == can_vote[0]:
+    winner = can_name[0]
+elif max(can_vote) == can_vote[1]:
+    winner = can_name[1]
+elif max(can_vote) == can_vote[2]:
+    winner = can_name[2]
+else:
+    winner = can_name[3]
+
+
+print("Election Results")
+print("--------------------------")
+print("Total Votes: " + str(total_vote))
+print("--------------------------")
+for i in range(4):
+    can_percent[i] = round(can_vote[i] * 100 / total_vote, 3)
+    print(can_name[i] + ": " + str(can_percent[i]) + "% (" + str(can_vote[i]) + ")")
+print("--------------------------")
+print("Winner: " + winner)
+print("--------------------------")
+
+result = zip(can_name, can_percent, can_vote)
+wrpath = os.path.join(".","election.csv")
+
+with open(wrpath, 'w', newline = '', encoding = "UTF-8") as datafile:
+
+    writer = csv.writer(datafile, delimiter=',')
+
+    writer.writerow(["Total Votes", total_vote, ""])
+    writer.writerows(result)
+    writer.writerow(["Winner", winner, ""])
